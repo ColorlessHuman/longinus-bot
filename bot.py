46086694c4c74661363cea016727002e458198de
Jyotirmoy Mandal <subho1301@gmail.com>
2018-10-27 17:34:14 +0530

    Write documentation
    Write inline documentation for the code in bot.py

--- bot.py
+++ bot.py
@@ -19,6 +19,9 @@ queues = {}
 
 
 def check_queue(id):
+    """
+    Check for queue items and play the next song in queue.
+    """
     if queues[id] != []:
         player = queues[id].pop(0)
         players[id] = player
@@ -27,6 +30,9 @@ def check_queue(id):
 
 @bot.event
 async def on_ready():
+    """
+    Print certain state details once the bot is started.
+    """
     print("Ready when you are!")
     print("I am running on " + bot.user.name)
     print("with ID " + bot.user.id)
@@ -34,22 +40,42 @@ async def on_ready():
 
 @bot.command(pass_context=True)
 async def ping(ctx):
+    """
+    Ping command
+
+    Returns Pong! on the discord message board.
+    """
     await bot.say("Pong!")
 
 
 @bot.command(pass_context=True)
 async def info(ctx, user: discord.Member):
+    """
+    Info command
+
+    Prints info of any user which is passed as an argument with this command.
+    """
     await bot.say("The username is: {}".format(user.name))
 
 
 @bot.command(pass_context=True)
 async def join(ctx):
+    """
+    Join command
+
+    This command makes the bot join the voice channel where the user is in.
+    """
     channel = ctx.message.author.voice.voice_channel
     await bot.join_voice_channel(channel)
 
 
 @bot.command(pass_context=True)
 async def leave(ctx):
+    """
+    Leave command
+
+    This commands makes the bot leave the voice channel it is in.
+    """
     server = ctx.message.server
     voice_client = bot.voice_client_in(server)
     await voice_client.disconnect()
@@ -57,6 +83,11 @@ async def leave(ctx):
 
 @bot.command(pass_context=True)
 async def play(ctx, url):
+    """
+    Play command
+
+    Plays audio content of youtube videos on a Discord voice channel.
+    """
     channel = ctx.message.author.voice.voice_channel
     await bot.join_voice_channel(channel)
     server = ctx.message.server
@@ -68,24 +99,44 @@ async def play(ctx, url):
 
 @bot.command(pass_context=True)
 async def pause(ctx):
+    """
+    Pause command
+
+    Pauses the player.
+    """
     id = ctx.message.server.id
     players[id].pause()
 
 
 @bot.command(pass_context=True)
 async def stop(ctx):
+    """
+    Stop command
+
+    Stops the player and clears queue.
+    """
     id = ctx.message.server.id
     players[id].stop()
 
 
 @bot.command(pass_context=True)
 async def resume(ctx):
+    """
+    Resume command
+
+    Resumes a paused player.
+    """
     id = ctx.message.server.id
     players[id].resume()
 
 
 @bot.command(pass_context=True)
 async def queue(ctx, url):
+    """
+    Queue command
+
+    Queues videos on the player.
+    """
     server = ctx.message.server
     voice_client = bot.voice_client_in(server)
     player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
@@ -96,5 +147,5 @@ async def queue(ctx, url):
         queues[server.id] = [player]
     await bot.say(f'Total {len(queues[server.id])} video(s) queued.')
 
-
+# Runs the bot with the discord bot token: TOKEN, which uniquely identifies your bot.
 bot.run(TOKEN)
